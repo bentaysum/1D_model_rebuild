23b09df1a678d9de9753e0b301a767a32eb188b0
btaysum <btaysum@googlemail.com>
2021-01-25 13:59:08 +0000

    Cl routine installed - connect with rest of model tracer by tracer

--- oneDmgcm/libf/aeronomars/inichim_newstart.F90
+++ oneDmgcm/libf/aeronomars/inichim_newstart.F90
@@ -1784,7 +1784,7 @@ ch4vmr=0.41275263e-9
       end if 
       ! HCl
       if (igcm_hcl /= 0) then
-         vmr = 0.
+         vmr = 100.e-9
          do i = 1,iip1
             do j = 1,jjp1
                do l = 1,llm
