64c92de82c5eb220b9a7bc29a8a11cd2453fbf21
btaysum <btaysum@googlemail.com>
2021-04-08 14:42:19 +0100

    Dust Storm MCD subsets in use

--- oneDmgcm/libf/aeronomars/tgo_profiles.F90
+++ oneDmgcm/libf/aeronomars/tgo_profiles.F90
@@ -1,6 +1,21 @@
-SUBROUTINE tgo_profiles()
+SUBROUTINE tgo_profiles(h2o,alt)
+
+USE HDF5 
 
 IMPLICIT NONE 
+
+#include "dimensions.h"
+#include "dimphys.h"
+#include "chimiedata.h"
+#include "tracer.h"
+#include "comcstfi.h"
+#include "callkeys.h"
+#include "conc.h"
+#include "netcdf.inc"
+#include "comgeomfi.h"
+
+REAL h2o
+REAL alt
 
 
 END 
